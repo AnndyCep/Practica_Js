daea3f55bc1ecf503f0e43ad9e0e485fad3fcbdb
Andres <hannsonandrey@hotmail.com>
2023-04-13 16:09:16 -0500

    Se agrega estructura de condicione while y for

---
+++ Unidad_6/EstructuraControl.js
@@ -0,0 +1,16 @@
+// While
+// Nos permite realizar una condicion hasta que se cumpla la condicion.
+
+let numero = 0
+
+while (numero <=10){
+    console.log(numero)
+    numero++
+}
+
+//for
+//Permite ejecutar un codigo un numero determinado de veces
+let num = 10
+for (let i = 0 ; i < num ; i++){
+    console.log(` el numero iterado es ${i}`)
+}
